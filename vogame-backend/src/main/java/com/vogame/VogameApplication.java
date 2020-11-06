739b96d19873c4e73760794f6637f5f4f6c6e95b
Grzegorz Rzepka <grzegorz.rzepka@codlake.com>
2020-11-06 14:51:40 +0100

    Add datasource file to local configuration

--- vogame-backend/src/main/java/com/vogame/VogameApplication.java
+++ vogame-backend/src/main/java/com/vogame/VogameApplication.java
@@ -2,8 +2,16 @@ package com.vogame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+@PropertySources(
+        {
+                @PropertySource("classpath:application.properties"),
+                @PropertySource(value = "classpath:datasource.properties", ignoreResourceNotFound = true)
+        }
+)
 public class VogameApplication {
 
     public static void main(String[] args) {
