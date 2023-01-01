bbb3e866fa3766a05894ba1ba2a888ea1a385194
Grzegorz Rzepka <grzegorz.rzepka@codlake.com>
2023-01-01 16:53:48 +0100

    Remove datasource.properties

--- vogame-backend/src/main/java/com/vogame/VogameApplication.java
+++ vogame-backend/src/main/java/com/vogame/VogameApplication.java
@@ -8,8 +8,7 @@ import org.springframework.context.annotation.PropertySources;
 @SpringBootApplication
 @PropertySources(
         {
-                @PropertySource("classpath:application.properties"),
-                @PropertySource(value = "classpath:datasource.properties", ignoreResourceNotFound = true)
+                @PropertySource("classpath:application.properties")
         }
 )
 public class VogameApplication {
